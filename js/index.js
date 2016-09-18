23d105d286e0a95bb54356f12a518af9774770bf
Angel <angel.mauricio.marcos@live.com>
2016-09-18 12:18:19 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function miFuncionHola()
+      {
+          alert('ola k dice');
+          var x = document.getElementById('miXspan');
+          var numA, numB;
+          alert(x.innerHTML);
+          x.innerHTML ='magia y no es pokemon';
+          
+          x.style.display = 'block';
+          
+          
+          numA = '5';
+          numB = 10;
+          
+          
+          numB = {1,3,5,7,9};
+          
+          x.innerHTML += numA + numB;
+          
+          x.innerHTML+= numB[2];
+          
+      }
+
+function suma()
+{
+    
+}
---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.dropdown-button').dropdown();
+    $('.modal-trigger').leanModal();
+    $('collapsible').collapsible();
+    $('.tooltipped').tooltip({delay: 50});
+    $('.materialboxdd').materialbox();
+    $('.slider').slider({full_width: true});
+
+  });
+})(jQuery);
